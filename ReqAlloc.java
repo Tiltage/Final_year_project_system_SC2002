1a99e1717f37cffbc094e1cb08c66ab6f40730d5
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-04-01 22:34:23 +0800

    Update ReqAlloc.java

--- ReqAlloc.java
+++ ReqAlloc.java
@@ -1,35 +1,32 @@
-package OOP;
+package project;
 
-public class ReqAlloc extends Request {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ReqAlloc extends Request{
+
+	public ReqAlloc(boolean approvalStatus, String studentID, String projTitle){
+		super(approvalStatus, studentID, projTitle);
+		this.setType(RequestType.ReqAlloc);
+	}
 	
-	  enum RegisterType {Register, Deregister}
+	public void addRequest()
+	{
+		final String FILENAME = "C:\\Users\\ryank\\Downloads\\request.csv";
+		try (FileWriter fw = new FileWriter(FILENAME, true);
+	             BufferedWriter bw = new BufferedWriter(fw);
+	             PrintWriter out = new PrintWriter(bw)) 
+		{
+	            // Add a new row to the bottom of the file
+	            out.printf("%s,%s,%s,%s", this.isApprovalStatus(), this.getType(), this.getStudentID(), this.getProjTitle()).println();
+	            System.out.println("Data appended to file successfully!");
 
-	  private String cood;
-	  private String sup;
-	  private RegisterType type;
-	  
-	  public String getCood() {
-	    return cood;
-	  }
-	  
-	  public void setCood(String cood) {
-	    this.cood = cood;
-	  }
-	  
-	  public String getSup() {
-	    return sup;
-	  }
-	  
-	  public void setSup(String sup) {
-	    this.sup = sup;
-	  }
-	  
-	  public RegisterType getType() {
-	    return type;
-	  }
-	  
-	  public void setType(RegisterType type) {
-	    this.type = type;
-	  }
-
+        } catch (IOException e) 
+			{
+	            e.printStackTrace();
+	        }
+	}
+	
 }
