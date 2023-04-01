07d924a05fd045336e1d4401502c9fa5e586964b
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-04-01 22:36:01 +0800

    Update ReqChangeTitle.java

--- ReqChangeTitle.java
+++ ReqChangeTitle.java
@@ -1,17 +1,49 @@
-package OOP;
+package project;
 
-public class ReqChangeTitle extends Request {
-	  
-	  private String changeTitle;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	  public String getChangeTitle() {
-	    return changeTitle;
-	  }
+public class ReqChangeTitle extends Request{
+	private String supID;
+	private String newProjTitle;
+	
+	public ReqChangeTitle(boolean approvalStatus, String studentID, String projTitle, String supID, String newProjTitle){
+		super(approvalStatus, studentID, projTitle);
+		this.setType(RequestType.ReqChangeTitle);
+		this.supID = supID;
+		this.newProjTitle = newProjTitle;
+	}
+	
+	public String getSupID() {
+		return supID;
+	}
+	public void setSupID(String supID) {
+		this.supID = supID;
+	}
+	public String getNewProjTitle() {
+		return newProjTitle;
+	}
+	public void setNewProjTitle(String newProjTitle) {
+		this.newProjTitle = newProjTitle;
+	}
+	
+	public void addRequest()
+	{
+		final String FILENAME = "C:\\Users\\ryank\\Downloads\\request.csv";
+		try (FileWriter fw = new FileWriter(FILENAME, true);
+	             BufferedWriter bw = new BufferedWriter(fw);
+	             PrintWriter out = new PrintWriter(bw)) 
+		{
+	            // Add a new row to the bottom of the file
+	            out.printf("%s,%s,%s,%s,%s,%s", this.isApprovalStatus(), this.getType(), this.getStudentID(), this.getProjTitle(),this.supID,this.newProjTitle).println();
+	            System.out.println("Data appended to file successfully!");
 
-	  public void setChangeTitle(String changeTitle) {
-	    this.changeTitle = changeTitle;
-	  }
-	
-	
-	
+        } catch (IOException e) 
+			{
+	            e.printStackTrace();
+	        }
+	}
+
 }
