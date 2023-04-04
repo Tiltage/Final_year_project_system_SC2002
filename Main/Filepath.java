8e408198c136f41ea52da810dd454a580ea1f94b
Tiltage <117619641+Tiltage@users.noreply.github.com>
2023-04-05 01:24:40 +0800

    Update Filename

--- Main/Filepath.java
+++ Main/Filepath.java
@@ -2,10 +2,32 @@ package Main;
 
 public class Filepath {
 	// Edit your filepath here manually
-	final String FILENAME = "C:\\Users\\Mel\\NTU\\Y1S2\\SC2002\\Test\\csvdemo.csv";
+	final String PROJFILENAME = "C:\\Users\\Mel\\NTU\\Y1S2\\SC2002\\rollover project.csv";
+	final String STUDFILENAME = "C:\\Users\\Mel\\NTU\\Y1S2\\SC2002\\student list.csv";
+	final String FACFILENAME = "C:\\Users\\Mel\\NTU\\Y1S2\\SC2002\\faculty_list.csv";
+	final String SUPFILENAME = "C:\\Users\\Mel\\NTU\\Y1S2\\SC2002\\faculty_list.csv";
+	final String REQFILENAME = "";
 
-	public String getPath() {
-		return FILENAME;
+	public String getPROJFILENAME() {
+		return PROJFILENAME;
 	}
+
+	public String getSTUDFILENAME() {
+		return STUDFILENAME;
+	}
+
+	public String getFACFILENAME() {
+		return FACFILENAME;
+	}
+
+	public String getSUPFILENAME() {
+		return SUPFILENAME;
+	}
+
+	public String getREQFILENAME() {
+		return REQFILENAME;
+	}
+	
+	
 
 }
