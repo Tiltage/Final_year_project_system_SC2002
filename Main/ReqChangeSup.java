9761bd816240a644663093de07644d9fd7030d24
Ryan Keh <113022074+ryankeh@users.noreply.github.com>
2023-04-10 13:34:59 +0800

    update to sup and reqchange sup

--- Main/ReqChangeSup.java
+++ Main/ReqChangeSup.java
@@ -1,9 +1,6 @@
 package Main;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class ReqChangeSup extends Request{
 	private String supName;
@@ -30,20 +27,22 @@ public class ReqChangeSup extends Request{
 	}
 	
 	public void addRequest()
-	{
-		Filepath f = new Filepath();
-		try (FileWriter fw = new FileWriter(f.REQFILENAME, true);
-	             BufferedWriter bw = new BufferedWriter(fw);
-	             PrintWriter out = new PrintWriter(bw)) 
-		{
-	            // Add a new row to the bottom of the file
-	            out.printf("%s,%s,%s,%s,%s,%s", this.isApprovalStatus(), this.getType(), this.getStudentID(), this.getProjTitle(),this.supName,this.newSupName).println();
-	            System.out.println("Data appended to file successfully!");
+	  {
+	    Filepath f = new Filepath();
+	    try (FileWriter fw = new FileWriter(f.getREQFILENAME(), true);
+	               BufferedReader br = new BufferedReader(new FileReader(f.getREQFILENAME()));
+	               PrintWriter out = new PrintWriter(fw)) 
+	    {
+	      
+	            br.close();
+	              // Add a new row to the bottom of the file
+	              out.printf("%s,%s,%s,%s,%s,%s", this.isApprovalStatus(), this.getType(), this.getStudentID(), this.getProjTitle(),this.supName,this.newSupName).println();
+	              System.out.println("Data appended to file successfully!");
 
-        } catch (IOException e) 
-			{
-	            e.printStackTrace();
-	        }
-	}
+	        } catch (IOException e) 
+	      {
+	              e.printStackTrace();
+	          }
+	  }
 
 }
