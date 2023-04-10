55a812b41bc82ce660eed462a433d4f5066760a5
Tiltage <117619641+Tiltage@users.noreply.github.com>
2023-04-10 13:44:16 +0800

    Update CoordinatorMenu.java

--- Main/CoordinatorMenu.java
+++ Main/CoordinatorMenu.java
@@ -10,42 +10,53 @@ public class CoordinatorMenu {
 	static String keyword="x";
 	static Scanner sc = new Scanner(System.in);
 	
-	
 	public static int display() {
 		
-		//Testing
-		
+		System.out.println("Supervisor menu page :");
+		System.out.println("1 : Create/View/Update Projects ");
+		System.out.println("2 : Approve/Reject/View Requests");
+		System.out.println("3 : Request to transfer student");
+		System.out.println("==========================");
 		System.out.println("Coordinator menu page :");
-		System.out.println("1 : Change password ");
-		System.out.println("2 : Create/Update/View your Projects ( AS COORDINATOR ) "); //supervisor
-		System.out.println("3 : Approve/Reject/View your Requests ( AS COORDINATOR )");
-		System.out.println("4 : Request to transfer student");
-		System.out.println("5 : View all Requests ( AS COORDINATOR )");
-		System.out.println("6 : View all projects ( AS COORDINATOR )");
+		System.out.println("4 : Change password ( AS COORDINATOR) ");
+		System.out.println("5 : Generate report( AS COORDINATOR ) "); //supervisor
+		System.out.println("6 : Approve/Reject/View your Requests ( AS COORDINATOR )");
 		System.out.println("7 : Quit");
 		choice=sc.nextInt();
 		
-		switch (choice) {
+		switch (choice) 
+		{
 		
-		case 2 : 
-
+		case 1: 
 			System.out.println("8 : Create new project ");
 			System.out.println("9 : View projects ");
 			System.out.println("10 : Update projects "); //to modify title himself
 			choice=sc.nextInt();
 			break;
 		
-		case 3 :
-			
-			System.out.println("11 : View pending requests "); // //includes new and untouched requests
+		case 2:	
+			System.out.println("11 : View Pending requests "); // //includes new and untouched requests
 			System.out.println("12 : View request history ");
 			System.out.println("13 : Approve requests "); //includes change title requests
 			System.out.println("14 : Reject requests ");
 			choice=sc.nextInt();
 			break;
+		case 5 : 
+			System.out.println("15 : View all projects ");
+			System.out.println("16 : ?????"); 
+			choice=sc.nextInt();
+			break;
 		
+		case 6 :
+			System.out.println("17 : View pending requests ");
+			System.out.println("18 : View request history ");
+			System.out.println("19 : Approve requests "); 
+			System.out.println("20 : Reject requests ");
+			choice=sc.nextInt();
+			break;
+			
 		default :
-		
+			System.out.println("Invalid option!");
 		}
 		return choice;
 	}
@@ -54,11 +65,12 @@ public class CoordinatorMenu {
 		
 		switch (c) {
 		
-		case 1 : 
-			co.changePW();
-			//if change password need quit and relogin to try again
-			System.out.println("Logging you out to retry password");
+		case 3 : 
+			co.requestTransfer();
 			break;
+			
+		case 4: 
+			co.changePW();
 		
 		case 8 : 
 			co.createProj();
@@ -88,32 +100,25 @@ public class CoordinatorMenu {
 			co.approveReq(false);
 			break;
 			
-		case 4 : 
-			co.requestTransfer();
+		case 15 : 
+			co.viewAllProjs();
 			break;
 			
-		case 5 : 
-			
-			//first we print out all the requests (maybe including history)
-			co.viewPendingReq();
+		case 16:
 			break;
-			//then we will prompt for either approve/reject
-			//then we call the functions
 			
+		case 17:
+			co.viewPendingReqCoord();
 			
+		case 18:
+			co.viewReqHistCoord();
 			
+		case 19:
+			co.approveReqCoord(true);
 			
-		
-			
-		case 6 : 
-			co.viewAllProjs(); //by title and availability
-			while (keyword.compareTo("0")!=0) {
-				System.out.println("Search by keywords (Enter 0 to quit) :");
-				keyword=sc.next();
-				co.viewProj();
-			}
-			
-			
+		case 20:
+			co.approveReqCoord(false)
+	
 			break;
 			
 			
